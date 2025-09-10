d36a8f08e2af2c7742e5b5261d70eae0d67ea21e
Larry Jones <mrwizard82d1@gmail.com>
2025-09-09 23:22:20 -0500

    Define routes using `compojure`

--- webdev/src/webdev/core.clj
+++ webdev/src/webdev/core.clj
@@ -1,28 +1,26 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.reload :refer [wrap-reload]]))
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [defroutes GET]]
+            [compojure.route :refer [not-found]]))
 
 (defn greet [req]
-  (cond
-    (= (:uri req) "/")
-    {:status 200
-     :body "Hello, Ring World (no not that \"Ringworld\")!"
-     :headers {}}
-    (= (:uri req) "/goodbye")
-    {:status 200
-     :body "Goobye, Cruel World!"
-     :headers {}}
-    :else
-    {:status 404
-     :body "Page not found"
-     :headers{}}))
+  {:status 200
+    :body "Hello, Ring World (no not that \"Ringworld\")!"
+    :headers {}})
+
+(defroutes app
+  ;; When a user requests the root, supply a friendly greeting
+  (GET "/" [] greet)
+  ;; No matches! Respond with a 404 and a "Page not found" message
+  (not-found "Page not found"))
 
 (defn -main [port]
   ;; The function, `jetty/run-jetty` creates a Jetty adapter for Ring. Ring
   ;; is the code that handles the request / response cycle but expects
   ;; requests to be in the form that Ring understands (using Clojure maps
   ;; and so on).
-  (jetty/run-jetty greet {:port (Integer. port)}))
+  (jetty/run-jetty app {:port (Integer. port)}))
 
 (defn -dev-main
   "This function is **only** called in development.
@@ -31,4 +29,4 @@
   wraps our key function, `greet`, in middleware that supports reloading
   thereby reducing the number of times one must restart the server"
   [port]
-  (jetty/run-jetty (wrap-reload #'greet) {:port (Integer. port)}))
+  (jetty/run-jetty (wrap-reload #'app) {:port (Integer. port)}))
