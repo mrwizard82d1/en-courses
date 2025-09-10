721f33148e2e2896a047602d898da8129a44f49c
Larry Jones <mrwizard82d1@gmail.com>
2025-09-09 22:56:39 -0500

    Add wrap-reload middleware for development

--- webdev/project.clj
+++ webdev/project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [ring/ring "1.15.0-RC1"]]
   :repl-options {:init-ns webdev.core}
-  :main webdev.core)
+  :main webdev.core
+  :profiles {:dev {:main webdev.core/-dev-main}})
--- webdev/src/webdev/core.clj
+++ webdev/src/webdev/core.clj
@@ -1,6 +1,6 @@
 (ns webdev.core
-  (:require [clojure.pprint :as pprint]
-            [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]))
 
 (defn greet [req]
   ;; The Ring response has three parts:
@@ -32,3 +32,12 @@
   ;; requests to be in the form that Ring understands (using Clojure maps
   ;; and so on).
   (jetty/run-jetty greet {:port (Integer. port)}))
+
+(defn -dev-main
+  "This function is **only** called in development.
+
+  The major difference between `-main` and `-dev-main`` is that `-dev-main`
+  wraps our key function, `greet`, in middleware that supports reloading
+  thereby reducing the number of times one must restart the server"
+  [port]
+  (jetty/run-jetty (wrap-reload #'greet) {:port (Integer. port)}))
