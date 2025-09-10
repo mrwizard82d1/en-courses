4f86801fb9ef79c590887339dfd1d6cd7059662b
Larry Jones <mrwizard82d1@gmail.com>
2025-09-09 23:10:17 -0500

    Add the compojure library as a dependency

--- webdev/project.clj
+++ webdev/project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring "1.15.0-RC1"]]
+                 [ring/ring "1.15.0-RC1"]
+                 [compojure "1.7.1"]]
   :repl-options {:init-ns webdev.core}
   :main webdev.core
   :profiles {:dev {:main webdev.core/-dev-main}})
